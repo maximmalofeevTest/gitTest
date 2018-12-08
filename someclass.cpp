d84d57d87821c2888cc95a7b5ff01252024e03fe
Maxim <maximMalofeev@bk.ru>
2018-12-08 20:03:37 +0300

    added some class

---
+++ someclass.cpp
@@ -0,0 +1,6 @@
+#include "someclass.h"
+
+SomeClass::SomeClass()
+{
+
+}
---
+++ someclass.h
@@ -0,0 +1,10 @@
+#ifndef SOMECLASS_H
+#define SOMECLASS_H
+
+class SomeClass
+{
+public:
+    SomeClass();
+};
+
+#endif // SOMECLASS_H
