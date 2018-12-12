087ae09c7f48cb23a20063a4f20d823ea507c037
Maxim <maximMalofeev@bk.ru>
2018-12-12 12:22:54 +0300

    f2 first feature

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char** argv)
 {
     std::cout << "==v1.1==" << std::endl;
+	std::cout << "F2 feature" << std::endl;
     std::cout << "Hello from main of " << argv[0] << " with " << argc << " argc:" << std::endl;
     for(auto i = 1; i <= argc; i++){
         std::cout << "#" << i << " = " << argv[i] << std::endl;
